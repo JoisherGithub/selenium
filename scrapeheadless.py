0da654481db3a3b0341896a3dde901dd38a02d67
ridzaky rasyaddin <167661522+JoisherGithub@users.noreply.github.com>
2024-09-17 08:58:41 +0700

    Create scrapeheadless.py

---
+++ scrapeheadless.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+
+chrome_options.add_argument("--headless")
+
+# disable google bot detection
+chrome_options.add_argument("--disable-web-security")
+chrome_options.add_argument("--allow-running-insecure-content")
+
+chrome_options.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"')
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.example.com")
+
+print(driver.title)
+
+driver.quit()
