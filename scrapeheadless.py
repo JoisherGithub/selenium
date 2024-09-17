79e0b9fc351b4d1cfb70d8d426ad843ace3a3b27
ridzaky rasyaddin <167661522+JoisherGithub@users.noreply.github.com>
2024-09-17 09:06:16 +0700

    Update scrapeheadless.py

--- scrapeheadless.py
+++ scrapeheadless.py
@@ -8,7 +8,6 @@ chrome_options.add_argument("--headless")
 # disable google bot detection
 chrome_options.add_argument("--disable-web-security")
 chrome_options.add_argument("--allow-running-insecure-content")
-
 chrome_options.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"')
 
 driver = webdriver.Chrome(options=chrome_options)
